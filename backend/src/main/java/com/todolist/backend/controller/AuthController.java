c17281c3dd2da2c1e3bd7ba8ec51a61a6dab6ff9
Krekno <yigiterenkarabas@gmail.com>
2025-07-09 01:39:53 +0300

    add AuthController

--- backend/src/main/java/com/todolist/backend/controller/AuthController.java
+++ backend/src/main/java/com/todolist/backend/controller/AuthController.java
@@ -1,5 +1,27 @@
 package com.todolist.backend.controller;
 
+import com.todolist.backend.model.dto.LoginRequest;
+import com.todolist.backend.model.dto.RegisterRequest;
+import com.todolist.backend.service.impl.AuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    private AuthServiceImpl authService;
+
+    @RequestMapping("/login")
+    public ResponseEntity<String> login(LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @RequestMapping("/register")
+    public ResponseEntity<String> register(RegisterRequest request) {
+        return authService.register(request);
+    }
 }
